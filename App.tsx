bf82c06163672baab5ef1ad7e0d422c6d61db38a
MACBOOK <ben10a2992@gmail.com>
2024-02-13 15:33:58 +0200

    Start develop in education app

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Welcome Back to develop react native apps</Text>
+    </View>
+  );
+};
+
+export default App;
